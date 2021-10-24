2ece15f3f1378b7483a12ad903ee90e341e19dde
covers1624 <laughlan.cov@internode.on.net>
2021-10-25 02:05:23 +1030

    Update dependencies, strip a few unneeded deps out.

--- src/intellij/groovy/net/covers1624/wt/intellij/api/script/Intellij.groovy
+++ src/intellij/groovy/net/covers1624/wt/intellij/api/script/Intellij.groovy
@@ -5,13 +5,13 @@ import net.covers1624.wt.api.script.Workspace
 /**
  * Created by covers1624 on 23/7/19.
  */
-interface Intellij extends Workspace {
+trait Intellij implements Workspace {
 
-    default void jdkName(String name) {
+    void jdkName(String name) {
         setJdkName(name)
     }
 
-    void setJdkName(String name)
+    abstract void setJdkName(String name)
 
-    String getJdkName()
+    abstract String getJdkName()
 }
